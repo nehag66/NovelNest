804bd20c7960a8f5200cd6182249ab3a706840bf
Neha Goel <nehagoel@Nehas-MacBook-Air.local>
2024-06-25 14:09:59 +0530

    header created, banner component creation in progress, prettier setup done

--- novelNest/src/app/app.component.spec.ts
+++ novelNest/src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'novelNest' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('novelNest');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, novelNest');
-  });
 });
--- novelNest/src/app/app.component.ts
+++ novelNest/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MaterialModule } from './material.module';
+import { BannerComponent } from "./banner/banner.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HeaderComponent, MaterialModule, BannerComponent]
 })
-export class AppComponent {
-  title = 'novelNest';
-}
+export class AppComponent {}
