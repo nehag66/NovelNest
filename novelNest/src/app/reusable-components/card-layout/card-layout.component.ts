0d2bdc60e0a20e9ef7cc0ad752cc68b1d8b3ab6e
Neha Goel <nehagoel66@gmail.com>
2024-06-28 20:52:40 +0530

    embedded youtube video in main page

--- novelNest/src/app/reusable-components/card-layout/card-layout.component.ts
+++ novelNest/src/app/reusable-components/card-layout/card-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { MaterialModule } from '../../material.module';
 import { CardDetails } from '../../main-page/sell-old-books/sell-old-books.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'app-card-layout',
 	standalone: true,
-	imports: [MaterialModule],
+	imports: [MaterialModule, CommonModule],
 	templateUrl: './card-layout.component.html',
 	styleUrl: './card-layout.component.scss',
 })
