cfbda25cd8a54fda2622ba9f1aaf5aaefcdec9a7
Neha Goel <nehagoel66@gmail.com>
2024-10-23 16:21:35 +0530

    worked on cart UI

--- novelNest/src/app/my-cart/my-cart.component.ts
+++ novelNest/src/app/my-cart/my-cart.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { CLIENT_ROUTES } from 'app/app.routes';
+import { LoginSignupDialogComponent } from 'app/login/login-signup-dialog/login-signup-dialog.component';
 import { MaterialModule } from 'app/material.module';
+import { SharedModule } from 'shared/shared.module';
 
 @Component({
 	selector: 'my-cart',
 	standalone: true,
-	imports: [MaterialModule],
+	imports: [MaterialModule, SharedModule],
 	templateUrl: './my-cart.component.html',
 	styleUrl: './my-cart.component.scss',
 })
-export class MyCartComponent {}
+export class MyCartComponent {
+	private readonly _dialog = inject(MatDialog);
+	private readonly _router = inject(Router);
+	cartItems = [];
+	isLoggedIn = false;
+
+	openLoginDialog(e: Event) {
+		e.preventDefault();
+		const dialogRef = this._dialog.open(LoginSignupDialogComponent, {
+			width: '400px',
+			height: '300px',
+			maxWidth: '80vw',
+			minWidth: '200px',
+		});
+		dialogRef.afterClosed().subscribe((result) => {});
+	}
+
+	goToBuyBooks() {
+		this._router.navigate([CLIENT_ROUTES.BUY_BOOKS]);
+	}
+}
