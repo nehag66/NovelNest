a7e51023d1a122de5bd118710328fb5f3c18a4a5
Neha Goel <nehagoel66@gmail.com>
2025-03-10 22:20:40 +0530

    carousel made the way it was supposed to be populated with books

--- novelNest/src/app/main-page/carousel/carousel.component.ts
+++ novelNest/src/app/main-page/carousel/carousel.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MaterialModule } from '../../material.module';
 import { Router } from '@angular/router';
 import { CLIENT_ROUTES } from 'app/app.routes';
+import { ApiService } from 'services/api.service';
+import { Novel } from 'app/models/novels';
 
 @Component({
 	selector: 'app-carousel',
@@ -12,40 +14,24 @@ import { CLIENT_ROUTES } from 'app/app.routes';
 	styleUrl: './carousel.component.scss',
 })
 export class CarouselComponent {
-	private readonly _router = inject(Router);
 	currentSlide = 0;
-	slides = [
-		{
-			image: 'carousel 1.jpg',
-			caption: 'Slide 1',
-		},
-		{
-			image: 'carousel 2.jpg',
-			caption: 'Slide 2',
-		},
-		{
-			image: 'carousel 3.jpg',
-			caption: 'Slide 3',
-		},
-		{
-			image: 'carousel 4.jpg',
-			caption: 'Slide 4',
-		},
-		{
-			image: 'carousel 5.jpg',
-			caption: 'Slide 5',
-		},
-	];
+	novels: Novel[] = [];
 
-	nextSlide() {
-		this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+	constructor(
+		private _router: Router,
+		private _apiService: ApiService,
+	) {
+		this._apiService.get('novels').subscribe((res: any) => {
+			this.novels = res.novels;
+		});
 	}
 
-	previousSlide() {
-		this.currentSlide =
-			(this.currentSlide - 1 + this.slides.length) % this.slides.length;
-	}
 	goToBuyBooks() {
 		this._router.navigate([CLIENT_ROUTES.BUY_BOOKS]);
+	}
+
+	scroll(amount: number) {
+		const carousel = document.querySelector('.carousel') as HTMLElement;
+		carousel.scrollBy({ left: amount, behavior: 'smooth' });
 	}
 }
