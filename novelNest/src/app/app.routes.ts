e10ed50e7c77529cbd52eb251b7734d34941345e
Neha Goel <nehagoel66@gmail.com>
2025-04-08 20:35:52 +0530

    tried implementing lazy loading in components, but it's not working correctly, so commented the approach for now

--- novelNest/src/app/app.routes.ts
+++ novelNest/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
-import { SellUsedBooksComponent } from './sell-used-books/sell-used-books.component';
-import { AllBooksComponent } from './all-books/all-books.component';
-import { MyCartComponent } from './my-cart/my-cart.component';
-import { NovelDetailsComponent } from './all-books/novel-details/novel-details.component';
-import { BuyNowComponent } from './buy-now/buy-now.component';
 import { AuthGuard } from 'shared/auth.guard';
+import { BuyNowComponent } from './buy-now/buy-now.component';
+import { NovelDetailsComponent } from './all-books/novel-details/novel-details.component';
+import { AllBooksComponent } from './all-books/all-books.component';
+import { SellUsedBooksComponent } from './sell-used-books/sell-used-books.component';
+import { MyCartComponent } from './my-cart/my-cart.component';
 
 export const CLIENT_ROUTES = {
 	MY_CART: 'cart',
@@ -21,41 +21,48 @@ export const CLIENT_ROUTES = {
 export const routes: Routes = [
 	{
 		path: CLIENT_ROUTES.MY_CART,
+		// loadChildren: () => import('./my-cart/my-cart.component').then(m => m.MyCartComponent),
 		component: MyCartComponent,
 		title: 'My Cart',
 		// canActivate: [AuthGuard],
 	},
 	{
 		path: CLIENT_ROUTES.SELL_USED_BOOKS,
+		// loadChildren: () => import('./sell-used-books/sell-used-books.component').then(m => m.SellUsedBooksComponent),
 		component: SellUsedBooksComponent,
 		title: 'Sell your used books for money',
 		// canActivate: [AuthGuard],
 	},
 	{
 		path: CLIENT_ROUTES.EDIT_NOVELS,
+		// loadChildren: () => import('./sell-used-books/sell-used-books.component').then(m => m.SellUsedBooksComponent),
 		component: SellUsedBooksComponent,
 		title: 'Edit Novel',
 		canActivate: [AuthGuard],
 	},
 	{
 		path: CLIENT_ROUTES.ALL_BOOKS,
+		// loadChildren: () => import('./all-books/all-books.component').then(m => m.AllBooksComponent),
 		component: AllBooksComponent,
 		title: 'Buy New, Old and second hand books',
 	},
 	{
 		path: CLIENT_ROUTES.NOVEL_DETAILS,
+		// loadChildren: () => import('./all-books/novel-details/novel-details.component').then(m => m.NovelDetailsComponent),
 		component: NovelDetailsComponent,
 		title: 'Novel Details',
 		canActivate: [AuthGuard],
 	},
 	{
 		path: CLIENT_ROUTES.BUY_BOOKS,
+		// loadChildren: () => import('./buy-now/buy-now.component').then(m => m.BuyNowComponent),
 		component: BuyNowComponent,
 		title: 'Buy Now',
 		canActivate: [AuthGuard]
 	},
 	{
 		path: CLIENT_ROUTES.MAIN_PAGE,
+		// loadChildren: () => import('./main-page/main-page.component').then(m => m.MainPageComponent),
 		component: MainPageComponent,
 		title: 'Novel Nest',
 	},
