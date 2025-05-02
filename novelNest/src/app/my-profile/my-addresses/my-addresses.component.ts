29890f1cc357a15313b3fa69795ca00b9a93a6fb
Neha Goel <nehagoel66@gmail.com>
2025-05-02 19:50:05 +0530

    my orders and my addresses page UI created

--- novelNest/src/app/my-profile/my-addresses/my-addresses.component.ts
+++ novelNest/src/app/my-profile/my-addresses/my-addresses.component.ts
@@ -1,10 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MaterialModule } from 'app/material.module';
+import { AuthService } from 'services/auth.service';
 
 @Component({
 	selector: 'app-my-addresses',
 	standalone: true,
-	imports: [],
+	imports: [CommonModule, MaterialModule],
 	templateUrl: './my-addresses.component.html',
 	styleUrl: './my-addresses.component.scss',
 })
-export class MyAddressesComponent {}
+export class MyAddressesComponent {
+	addresses = [];
+	isLoggedIn: string | null = null;
+
+	constructor(private _authService: AuthService) {
+		this.isLoggedIn = this._authService.bearerToken;
+	}
+}
--- novelNest/src/app/my-profile/my-orders/my-orders.component.ts
+++ novelNest/src/app/my-profile/my-orders/my-orders.component.ts
@@ -1,10 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CLIENT_ROUTES } from 'app/app.routes';
+import { MaterialModule } from 'app/material.module';
+import { AuthService } from 'services/auth.service';
 
 @Component({
 	selector: 'app-my-orders',
 	standalone: true,
-	imports: [],
+	imports: [CommonModule, MaterialModule],
 	templateUrl: './my-orders.component.html',
 	styleUrl: './my-orders.component.scss',
 })
-export class MyOrdersComponent {}
+export class MyOrdersComponent {
+	orders = [];
+	isLoggedIn: string | null = null;
+
+	constructor(private _router: Router, private _authService: AuthService) {
+		this.isLoggedIn = this._authService.bearerToken;
+	}
+
+	goToBuyBooks() {
+		this._router.navigate([CLIENT_ROUTES.ALL_BOOKS]);
+	}
+}
