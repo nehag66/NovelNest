737f8c27e00d11a8ca80c9bae344d97fd3bc785b
Neha Goel <nehagoel66@gmail.com>
2025-06-03 20:37:25 +0530

    author, categories and userinfo cached on main-page component

--- novelNest/src/app/main-page/main-page.component.ts
+++ novelNest/src/app/main-page/main-page.component.ts
@@ -8,6 +8,9 @@ import { CarouselComponent } from './carousel/carousel.component';
 import { StorageService } from 'services/storage.service';
 import { ApiService } from 'services/api.service';
 import { AuthorResponse, AuthorSummary } from 'app/models/author';
+import { Category, CategoryResponse } from 'app/models/novels';
+import { ProfileResponse } from 'app/models/profile';
+import { AuthService } from 'services/auth.service';
 
 @Component({
 	selector: 'app-main-page',
@@ -24,22 +27,69 @@ import { AuthorResponse, AuthorSummary } from 'app/models/author';
 	styleUrl: './main-page.component.scss',
 })
 export class MainPageComponent implements OnInit {
+	userId: string | null = '';
 	constructor(
 		private _storageService: StorageService,
 		private _apiService: ApiService,
+		private _authService: AuthService,
 	) {}
 
 	ngOnInit() {
-		// TODO: Add categories to localstorage too
+		this._authService.loginStatusChanged.subscribe(() => {
+			this.afterLogin();
+		});
+	}
+
+	afterLogin() {
+		this.userId = this._storageService.get<string>('userId');
+		this.cacheUserInfo();
+		this.cacheCategories();
+		this.cacheAuthors();
+	}
+
+	cacheUserInfo() {
+		const userInfo = this._storageService.get('userInfo');
+		if (!userInfo) {
+			this._apiService
+				.get<{
+					message: string;
+					user: ProfileResponse;
+				}>(`users/${this.userId}`)
+				.subscribe(
+					(userInfo: { message: string; user: ProfileResponse }) => {
+						this._storageService.set('userInfo', userInfo.user);
+					},
+				);
+		}
+	}
+
+	cacheAuthors() {
 		const authors = this._storageService.get('authors');
 		if (!authors) {
 			this._apiService
 				.get<{
 					message: string;
 					authors: AuthorSummary[];
-				}>(`authors`)
+				}>('authors')
 				.subscribe((authors: AuthorResponse) => {
 					this._storageService.set('authors', authors.authors);
+				});
+		}
+	}
+
+	cacheCategories() {
+		const categories = this._storageService.get('categories');
+		if (!categories) {
+			this._apiService
+				.get<{
+					message: string;
+					categories: Category[];
+				}>('categories')
+				.subscribe((categories: CategoryResponse) => {
+					this._storageService.set(
+						'categories',
+						categories.categories,
+					);
 				});
 		}
 	}
