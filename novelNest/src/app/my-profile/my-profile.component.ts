63db355bf03b2fd6fea807e85627071c315251e8
Neha Goel <nehagoel66@gmail.com>
2025-06-05 15:28:43 +0530

    author and category dialog added to my-profile if role is superadmin

--- novelNest/src/app/my-profile/my-profile.component.ts
+++ novelNest/src/app/my-profile/my-profile.component.ts
@@ -1,17 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { CLIENT_ROUTES } from 'app/app.routes';
 import { MaterialModule } from 'app/material.module';
+import { AddCategoryDialogComponent } from './add-category-dialog/add-category-dialog.component';
+import { StorageService } from 'services/storage.service';
+import { CommonModule } from '@angular/common';
+import { UserInfo } from 'app/models/user';
+import { AddAuthorDialogComponent } from './add-author-dialog/add-author-dialog.component';
 
 @Component({
 	selector: 'app-my-profile',
 	standalone: true,
-	imports: [MaterialModule],
+	imports: [MaterialModule, CommonModule],
 	templateUrl: './my-profile.component.html',
 	styleUrl: './my-profile.component.scss',
 })
-export class MyProfileComponent {
-	constructor(private _router: Router) {}
+export class MyProfileComponent implements OnInit {
+	userInfo: UserInfo | null = {
+		name: '',
+		email: '',
+		address: '',
+		role: '',
+	};
+
+	constructor(
+		private _router: Router,
+		private _dialogService: MatDialog,
+		private _storageService: StorageService,
+	) {}
+
+	ngOnInit(): void {
+		this.userInfo = this._storageService.get('userInfo');
+	}
+
+	addCategoryDialog() {
+		const dialogRef = this._dialogService.open(AddCategoryDialogComponent, {
+			width: '400px',
+			height: '350px',
+			maxWidth: '80vw',
+			minWidth: '200px',
+			panelClass: 'custom-dialog',
+		});
+		dialogRef.afterClosed().subscribe();
+	}
+
+	addAuthorDialog() {
+		const dialogRef = this._dialogService.open(AddAuthorDialogComponent, {
+			width: '400px',
+			height: '350px',
+			maxWidth: '80vw',
+			minWidth: '200px',
+			panelClass: 'custom-dialog',
+		});
+		dialogRef.afterClosed().subscribe();
+	}
 
 	goToMyProfile() {
 		this._router.navigateByUrl(CLIENT_ROUTES.BASIC_INFO);
