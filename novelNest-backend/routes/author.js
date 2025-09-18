6af4840d46df5b6443f8b5201fa91f07c55bb6f6
Neha Goel <nehagoel66@gmail.com>
2025-09-18 21:26:42 +0530

    added superadmin role required for adding or deleting authors

--- novelNest-backend/routes/author.js
+++ novelNest-backend/routes/author.js
@@ -3,12 +3,29 @@ const router = express.Router();
 
 const authorController = require('../controllers/author');
 const verifyToken = require('../middlewares/verifyToken');
+const requireRole = require('../middlewares/requireRole');
 
 router.get('/author-details', authorController.getAllAuthorDetails);
 router.get('/authors', authorController.getAllAuthors);
 router.get('/authors/:id', authorController.getAuthorDetails);
-router.post('/authors', verifyToken, authorController.postAddAuthor);
-router.put('/authors/:id', verifyToken, authorController.editAuthor);
-router.delete('/authors/:id', verifyToken, authorController.deleteAuthor);
+router.post(
+	'/authors',
+	verifyToken,
+	requireRole(['superadmin']),
+	authorController.postAddAuthor,
+);
+// router.post('/authors', verifyToken, authorController.postAddAuthor);
+router.put(
+	'/authors/:id',
+	verifyToken,
+	requireRole(['superadmin']),
+	authorController.editAuthor,
+);
+router.delete(
+	'/authors/:id',
+	verifyToken,
+	requireRole(['superadmin']),
+	authorController.deleteAuthor,
+);
 
 module.exports = router;
