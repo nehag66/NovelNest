3cfeee47d4bc675cb0034894dc3e54be05b76e8e
Neha Goel <nehagoel66@gmail.com>
2025-09-20 20:25:26 +0530

    order service created in frontend

---
+++ novelNest/src/services/order.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { OrderResponse, Order, OrdersResponse } from 'app/models/order';
+import { Observable } from 'rxjs';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class OrderService {
+	constructor(private _apiService: ApiService) {}
+
+	createOrder(orderData: {
+		items: any[];
+		totalAmount: number;
+		paymentId?: string;
+	}): Observable<OrderResponse> {
+		return this._apiService.post<OrderResponse>('orders', orderData);
+	}
+
+	getUserOrders(): Observable<OrdersResponse> {
+		return this._apiService.get<OrdersResponse>('orders');
+	}
+}
